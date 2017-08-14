99aec92698505b5faee0399e8f03fa5f96178062
Ayush Tanna <tannayush@gmail.com>
2017-08-14 23:01:01 +0530

    Changed App Delegate

--- BirlaHighSchool/AppDelegate.swift
+++ BirlaHighSchool/AppDelegate.swift
@@ -18,7 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print("Application did finish launching")
+        
         FIRApp.configure()
         return true
     }
